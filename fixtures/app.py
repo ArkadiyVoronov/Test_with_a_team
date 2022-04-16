079f42611c7b1ef051ccbe9b389feefffcf8862d
Arkadiy Voronov <voronov_ao@mail.ru>
2022-04-16 12:16:03 +0500

    Revert "add tests balance"

--- fixtures/app.py
+++ fixtures/app.py
@@ -3,7 +3,6 @@ from fixtures.pages.basket_page import BasketPage
 from fixtures.pages.login_page import LoginPage
 from fixtures.pages.main_page import MainPage
 from fixtures.pages.register_page import RegisterPage
-from fixtures.pages.balance_page import BalancePage
 
 
 class Application:
--- fixtures/constants.py
+++ fixtures/constants.py
@@ -7,8 +7,3 @@ class Notice:
     ERROR_INVALID_EMAIL = "Error, test is not email address!"
     ERROR_PASS_TOO_SHORT = "Password must be longer than 7 characters"
     ERROR_DIFFERENT_PASS = "Error, passwords do not match!"
-    ERROR_NAME_BALANCE = "Check user name and last name! It must be not empty!"
-    ERROR_CARD_NUMBER = "Check card number! It must be 16 symbols and not empty!"
-    ERROR_CARD_DATE = "Check card date! It must be not empty!"
-    ERROR_CARD_MONEY = "Check money! It must be not empty!"
-    ERROR_CARD_CHECKBOX = "Read agree and click checkbox!"
